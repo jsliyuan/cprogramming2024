0692e6965b1c034ed9cb2b20987069077606d3ec
jsliyuan <jsliyuan@gmail.com>
2024-09-09 10:15:17 +0800

    Create oct_hex.c

---
+++ Week2/oct_hex.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  /* Input an integer N. Print its 
+     octal and hexadecimal representations.
+     E.g.
+     Input: 77
+     Ouput: 4d 115
+  */
+  return 1;
+}
