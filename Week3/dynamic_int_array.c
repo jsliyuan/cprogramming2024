6101b68053ee32e5d8e8a5368e05ee614e35272b
jsliyuan <jsliyuan@gmail.com>
2024-09-14 10:08:34 +0800

    Week3

---
+++ Week3/dynamic_int_array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // 1. Dynamically create an int type array of length n, where
+  //    n = 1000 000 000.
+  // 2. Set all elements of the array to the constant 1.
+  // 3. Free the array at the end.
+  return 0;
+}
---
+++ Week3/swap.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <assert.h>
+
+typedef void (*fun_t)(double*, double*);
+
+// Swap two doubles
+void swap(double *x, double *y) {
+  // Write your code here.
+}
+
+void TestSwapCase1(fun_t swap) {
+  double x = 1.00;
+  double y = 1.00;
+  swap(&x, &y);
+  assert(x == 1.00);
+  assert(y == 1.00);
+  printf("Passed %s\n", __func__);
+}
+
+void TestSwapCase2(fun_t swap) {
+  double x = 1.00;
+  double y = 2.00;
+  swap(&x, &y);
+  assert(x == 2.00);
+  assert(y == 1.00);
+  printf("Passed %s\n", __func__);
+}
+
+int main() {
+  TestSwapCase1(swap);
+  TestSwapCase2(swap);
+  return 0;
+}
