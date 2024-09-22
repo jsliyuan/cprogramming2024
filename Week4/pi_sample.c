4dd1bd652417ba29bd75b26155bee714ae7955f5
jsliyuan <jsliyuan@gmail.com>
2024-09-22 11:51:13 +0800

    Create pi_sample.c

---
+++ Week4/pi_sample.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+Sample a random point (x, y) in the square [-1, 1] * [-1, 1].
+There is a unit circle C centered on (0, 0) with a radius of 1.
+The probability that a randomly chosen point falls inside
+the circle C is exactly PI / 4.
+
+Estimate the value of PI with a precision of 1e-3 by sampling
+a sufficient number of points.
+*/
+int main() {
+  return 0;
+}
