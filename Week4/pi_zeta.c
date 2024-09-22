c36bc5cfefd603c4b1166b283ab460ded567c459
jsliyuan <jsliyuan@gmail.com>
2024-09-22 11:45:08 +0800

    Create pi_zeta.c

---
+++ Week4/pi_zeta.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+Let zeta(s) = 1/1^s + 1/2^s + 1/3^s + ...
+It is known that
+  zeta(2) = PI^2 / 6
+  zeta(4) = PI^4 / 90
+
+The goal is to compute the constant PI to a precision of 1e-3
+(meaning three decimal places) using the Riemann zeta function.
+*/
+
+int main() {
+  return 0;
+}
