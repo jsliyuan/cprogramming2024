1ddb509f1f908b1ae59ca94e98c8253f2ad14855
jsliyuan <jsliyuan@gmail.com>
2024-09-29 17:36:24 +0800

    lab4\cubic

---
+++ lab4/cubic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+
+// Solve the cubic equation x^3 + c2*x^2 + c1*x + c0 = 0 with a given precision eps.
+// The number of real solutions is stored in *cnt, where *cnt can be 1, 2, or 3.
+// The solutions are stored in sol[0], ..., sol[*cnt - 1].
+void solve_cubic_equ(double c2, double c1, double c0, double eps, int* cnt, double* sol) {
+	
+}
+
+int main() {
+
+  return 0;
+}
