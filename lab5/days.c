01a99849bade72a2707ccfa73d07fc2c4bb3d3af
jsliyuan <jsliyuan@gmail.com>
2024-10-04 15:15:10 +0800

    lab5

---
+++ lab5/days.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Given a specific date (year, month, day), this function
+// and returns the total number of days that have passed
+// since January 1st, Year 1.
+int date_to_days(int year, int month, int day) {
+
+}
+
+int main() {
+  return 0;
+}
