dfe1ccfbac8c8bc8f08935f5f97c1548078a1648
jsliyuan <jsliyuan@gmail.com>
2024-10-12 21:39:27 +0800

    lab5

---
+++ lab5/goldbach.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+Goldbach's conjecture states that every even natural
+number greater than 2 is the sum of two prime numbers.
+
+Given an even natural number greater than 2, find two
+primes (*prime1) and (*prime2) such that
+n = (*prime1) + (*prime2).
+*/
+void sum_of_two_primes(int n, int* prime1, int* prime2) {
+
+}
+
+int main() {
+  return 0;
+}
---
+++ lab5/sum_of_squares.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Given a positive integer n, write n as a sum of k squares,
+where k <= 4, that is, n = a1^2 + ... + ak^2, making k 
+as small as possible.
+*/
+void sum_of_squares(int n, int* k, int* list) {
+
+}
+
+int main() {
+  return 0;
+}
