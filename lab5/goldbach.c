b237fca6fc95a84166e2ee15815e1f8637dd32d0
Jason-wan23 <71265615+Jason-wan23@users.noreply.github.com>
2024-10-13 16:40:24 +0800

    add several assertion statements

--- lab5/goldbach.c
+++ lab5/goldbach.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <assert.h>
 /*
 Goldbach's conjecture states that every even natural
 number greater than 2 is the sum of two prime numbers.
@@ -12,6 +12,37 @@ void sum_of_two_primes(int n, int* prime1, int* prime2) {
 
 }
 
+
+    
+
+    
+void TestCase1() {
+  // Test case 1: 10 = 3 + 7
+  int prime1, prime2;
+  sum_of_two_primes(10, &prime1, &prime2);
+  assert((prime1 == 3 && prime2 == 7) || (prime1 == 7 && prime2 == 3));
+  printf("Passed TestCase1\n");
+}
+
+void TestCase2() {
+  // Test case 2: 28 = 11 + 17
+  int prime1, prime2;
+  sum_of_two_primes(28, &prime1, &prime2);
+  assert((prime1 == 11 && prime2 == 17) || (prime1 == 17 && prime2 == 11));
+  printf("Passed TestCase2\n");
+}
+
+void TestCase3() {
+  // Test case 3: 56 = 19 + 37
+  int prime1, prime2;
+  sum_of_two_primes(56, &prime1, &prime2);
+  assert((prime1 == 19 && prime2 == 37) || (prime1 == 37 && prime2 == 19));
+  printf("Passed TestCase3\n");
+}
+
 int main() {
+  TestCase1();
+  TestCase2();
+  TestCase3();
   return 0;
 }
--- lab5/sum_of_squares.c
+++ lab5/sum_of_squares.c
@@ -1,13 +1,54 @@
 #include <stdio.h>
-
+#include <math.h>
+#include <assert.h>
 /* Given a positive integer n, write n as a sum of k squares,
 where k <= 4, that is, n = a1^2 + ... + ak^2, making k 
 as small as possible.
 */
 void sum_of_squares(int n, int* k, int* list) {
+  
+}
+void TestCase1() {
+  // Test Case 1: 99999 (315^2 + 27^2 + 6^2 + 3^2)
+  int k ;
+  int list[4] ;
+  sum_of_squares(99999, &k, list);
+  assert(k == 4);
+  assert(list[0] == 315);
+  assert(list[1] == 27);
+  assert(list[2] == 6);
+  assert(list[3] == 3);
+  printf("Passed TestCase1\n");
+}
 
+void TestCase2() {
+  // Test Case 1: 87352 (294^2 + 30^2 + 4^2 )
+  int k ;
+  int list[4] ;
+  sum_of_squares(87352, &k, list);
+  assert(k == 3);
+  assert(list[0] == 294);
+  assert(list[1] == 30);
+  assert(list[2] == 4);
+  printf("Passed TestCase2\n");
+}
+
+void TestCase3() {
+  // Test Case 3: 76543 (275^2 + 30^2 + 3^2 + 3^2)
+  int k ;
+  int list[4] ;
+  sum_of_squares(57, &k, list);
+  assert(k == 4);
+  assert(list[0] == 6);
+  assert(list[1] == 4);
+  assert(list[2] == 2);
+  assert(list[3] == 1);
+  printf("Passed TestCase3\n");
 }
 
 int main() {
+  TestCase1();
+  TestCase2();
+  TestCase3();
   return 0;
 }
